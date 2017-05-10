4a7a5f0f35067b61dbb969bd5c94789a056181f8
Ken Supernova <ghzhang66@gmail.com>
2017-05-10 14:57:44 +0800

    大整理，大修复。

--- exchcard/urls.py
+++ exchcard/urls.py
@@ -1,11 +1,11 @@
 #coding:utf-8
 from django.conf.urls import include, url
 
-
-# Uncomment the next two lines to enable the admin:
+# To enable the admin:
 from django.contrib import admin
-## admin.exchard.admin_view("Exchange Postcard Administration Site")
 admin.autodiscover()
+
+
 
 urlpatterns = [
     url(r'^$', 'exchcard.views.index'),
@@ -13,22 +13,44 @@ urlpatterns = [
     url(r'^account/register/$', 'exchcard.views.user_register'),
     url(r'^account/resetpassword/$', 'exchcard.views.profile'),
     url(r'^account/findpassword/$', 'exchcard.views.profile'),
+    url(r'^account/setting/$', 'exchcard.views.profile'),
 
+    # view the current logged user's profile
     url(r'^profile/$', 'exchcard.views.profile'),
+
+    # send a card
     url(r'^card/send/$', 'exchcard.views.card_send'),
     url(r'^card/send/confirm/$', 'exchcard.views.card_send_confirm'),
+    # 查看还在路途中的某张明信片
     url(r'^card/travelling/(?P<cardname>.+)/$', 'exchcard.views.card_travelling'),
-    url(r'^card/register/$', 'exchcard.views.card_register'),
+    # register a card
+    url(r'^card/receive/$', 'exchcard.views.card_receive'),
+    # view a card with cardname
     url(r'^card/(?P<cardname>.+)/$', 'exchcard.views.view_single_card'),
+
+    # view current logged user's cards list
     url(r'^profile/(?P<id>.+)/cards/$', 'exchcard.views.view_cards_list'),
 
+    # view other's public profile page
+    url(r'^u/(?P<username>.+)/cards/$', 'exchcard.views.view_others_public_profile'),
+
+    # 发烧友圈子
     url(r'^moments/$', 'exchcard.views.view_shao_you_quan'),
 
-    url(r'^search/$', 'exchcard.views_search.search'),
-    url(r"^storage/s3/$", "exchcard_backend_api.upload_views.s3_storage"),
 
+    # 后台管理
     url(r'^admin/', include(admin.site.urls)),
+
+    # 前后端分离设计，这是后端API设计，基于rest framework
     url(r'^exchcard/api/', include('exchcard_backend_api.urls')),
     url(r'^oneverse/api/', include('oneverse_api.urls')),
+
+    # oneverse的url
+    url(r'^oneverse/', include('oneverse.urls')),
+
+    # 其他
+    url(r'^search/$', 'exchcard.views_search.search'),
+
+
 
 ]
