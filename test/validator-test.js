100e74139e427b857e3553f0f652b9d7c57457d1
Ken Supernova <ghzhang66@gmail.com>
2017-06-20 09:59:49 +0800

    filter new files

---
+++ test/validator-test.js
@@ -0,0 +1,16 @@
+console.log(validate_password("1111111a"));
+console.log(validate_password("1111111a"));
+console.log(validate_password("zgh4@126"));
+
+console.log(validate_name("zghfafafa"))
+console.log(validate_name("  "));
+console.log(validate_name("aaaaaaa1"));
+console.log(validate_name(".."));
+console.log(validate_name("张."));
+// console.log(validate_name("张.张"));
+// console.log(validate_name("a.a"));
+// console.log(validate_name("a a"));
+
+
+// console.log(validate_address("."))
+// console.log(validate_address("10000"))
